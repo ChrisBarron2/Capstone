7fe3c7fb3af07075584b474ab890a53804df0946
COR Students <corstudents@Matts-MBP-9.localdomain>
2018-11-09 13:25:46 -0500

    Capstone

---
+++ Objects- 2/Objects/objects.js
@@ -0,0 +1,135 @@
+//Ojects are a collectiopn of properties used to used to sotre data and can be used for a varity of things.
+
+
+// Step 1 define the class
+
+// class Student{
+//     //set up the instructor 
+
+// constructor(_name,_projectGrade,_testeGrade){
+//      console.log("Student is being created")
+
+
+
+// // define the objects properties usin the attributes from the instructor
+
+// this.name = _name,
+// this.projectGrade = _projectGrade,
+// this.testGrade = _testGrade 
+
+// }
+// //Define methods
+// getAverage() {
+// return(this.projectGrade + this.testGrade)/2
+
+
+// } 
+//     printsDetails(){
+// console.log(`My name is ${this.name} I'm a Dev in tranning. My project grade is ${this.projectGrade} my test grade is ${this.testGrade}`)
+// }
+// }
+
+// //class created new instance
+// let Student1 = new Student ("Chris", 90,85, 89)
+// let Student2 = new Student ("Jane", 100, 95, 87)
+// let Student3 = new Student ("Wonka", 85, 90.75)
+// // student1 = function () {
+// //     return (`I'm ${this.age}, I'm dev in trainning!`)
+// // } 
+
+// //delete  function
+
+// Head two legs and 
+
+
+// class Person { 
+//     contructor( _name) {
+//         this.name + _name;
+
+//         sayHello() {
+//             console.log ("Hello ! My name is $(this.name"))
+//         }
+//     }
+// }
+//  let names = ["devens", chris, "ray ", "jon", "kamaria"]
+//  for(i in people ) {
+
+
+
+//  }
+
+
+// class Person {
+//     constructor (_favoriteColors) {
+//     this._favoriteColors = _favoriteColors
+    
+// }
+
+// }
+
+// class Color {
+//    contructor(_name) {
+//        this.name =_name
+//    }
+ 
+//    sayFavoriteColors() {
+//  for (let  i  in this._favoriteColorsfavoriteColors) {
+// console.log(this._favoriteColors[i])
+
+// }
+
+
+// let colors = [new Color("red"), new Color("blue"), new Color("green"),new Color("orange"),
+// new colors ("orange")];
+// let fred = new Person(color);
+// fred.sayFavoriteColors();
+
+
+
+// const DiceRoll = {
+//     D6 () {
+//     console.log("testing the D6")    
+//    return Math.random() *6 + 1
+//     }
+// }
+
+// console.log("testing the page")
+// documnet .write(DiceRoll.D6())
+
+const DiceRoll = {
+    D6 (){
+        console.log("testing the D6")
+        return Math.floor(Math.random()*6 + 1)
+    }
+}
+    console.log("testing the page")
+     document.write(DiceRoll.D6())
+
+
+
+
+
+
+    // for(i in people); {
+    //     people[i].sayHello()
+    //     console.log("Hello");
+    // }
+
+    // // class Animal {
+    // //  constructor(_name, numberOflegs) {
+    // //  this.name =_name,
+    // //  this.numberOfLegs = numberOfLegs 
+
+
+    // //  }
+
+    // // }
+    // // let animalNames = ["Dogs", "Octopus", "Kangarro", "Dolphin", "Iguana", "starfish", centeriped];
+
+    // let amountOfLegs = [4,8,2,0,5,100] 
+
+    // let animals = []
+
+    // let dog = new Animal(animal) 
+
+    Put a button on a web and it will ChannelMergerNode.
