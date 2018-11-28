21454b87fd93b085edc4d0118ecd5700d1aadc7f
COR Students <corstudents@Matts-MBP-9.localdomain>
2018-11-28 10:49:48 -0500

    firstexpressprojects

--- firstexpressprojects/app.js
+++ firstexpressprojects/app.js
@@ -1,6 +1,31 @@
 const express = require('express');
+//require looks for packages for express
 const app = express();
 
 
 app.get('/',(req, res) => res.send('Hello World'))
+// app.get request for a particular function
 app.listen(3000, () => console.log('server ready'))
+
+
+app.get('/help',(req, res) => res.send('Hello World'))
+// app.get request for a particular function
+app.listen(3000, () => console.log('server ready'))
+
+app.get('/contact',(req, res) => res.send('Hello World'))
+// app.get request for a particular function
+app.listen(3000, () => console.log('server ready'))
+
+app.get('/contact us',(req, res) => res.send('Hello World'))
+// app.get request for a particular function
+app.listen(3000, () => console.log('server ready'))
+
+app.get('/find us',(req, res) => res.send('Hello World'))
+// app.get request for a particular function
+app.listen(3000, () => console.log('server ready'))
+
+
+
+
+
+
